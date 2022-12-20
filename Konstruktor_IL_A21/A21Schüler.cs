5b3c38fd686a2f32c363c2c32082bdf28d75b4d1
Elijah Nhek <Nhek.Elijah@bene-edu.ch>
2022-12-20 10:50:37 +0100

    Add project files.

---
+++ Konstruktor_IL_A21/A21Schüler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konstruktor_IL_A21
+{
+    internal class A21Schüler
+    {
+        public string fach { get; set; }
+        public double note { get; set; }
+
+        public A21Schüler(string f,double no)
+        {
+            fach = f; note = no; 
+        }
+        public void NoteÄnderung()
+        {
+            try
+            {
+                Console.Write("\nGeben Sie die neue Note ein: ");
+                double n = Convert.ToDouble(Console.ReadLine());
+                if(n>=1.0 && n <= 6.0)
+                {
+                    note = n;
+                }
+                else
+                {
+                    Console.WriteLine($"\n{n} Note nicht möchlich (Falsche Eingabe)");
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Konstruktor_IL_A21/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konstruktor_IL_A21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //intro
+            Console.WriteLine  
+            ("------------------------------------------------------------------------" +
+           "\n|    Willkommen zu unserem Informatiker Noten Durchschnitt berechner   |" +
+           "\n|----------------------------------------------------------------------|" +
+           "\n| In diesem Program können Sie Ihre Noten einschreiben und abspeichern |" +
+           "\n|                Drücken Sie *Enter*, um fortzufahren                  |" +
+           "\n------------------------------------------------------------------------");
+            Console.ReadLine();
+
+
+
+            //Program
+            there:
+            int x = 0;
+            GArray(ref x);
+            Console.WriteLine();
+            A21Schüler[] listeS = new A21Schüler[x];
+            for(int i=0; i<listeS.Length; i++)
+            {
+                Console.Write("Fach: ");
+                string fa = Console.ReadLine();
+                Console.Write("\nNote: ");
+                double no = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("------------------------------------------");
+                A21Schüler a21 = new A21Schüler(fa, no);
+                listeS[i] = a21;
+                Console.Clear();
+            }
+            Console.WriteLine("Ihre Noten:");
+            foreach (var c in listeS)
+            {
+                Console.WriteLine("---------------------------------------------");
+                Console.WriteLine($"Fach:    {c.fach}\n" +
+                                  $"Note:    {c.note}");
+                Console.WriteLine("---------------------------------------------");
+            }
+
+            Console.WriteLine("\nMöchten Sie Ihre Module neu einschreiben? (j/n)");
+            string choice = Console.ReadLine();
+            if (choice == "j")
+            {
+                Console.Clear();
+                goto there;
+            }
+            else
+            {
+                Console.WriteLine("\n\n\nMöchten Sie das Programm beenden? (j/n)");
+                string choice2 = Console.ReadLine();
+                if (choice2 == "j")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Schönen Tag!");
+                }
+                else
+                {
+                    Console.ReadKey();
+                }
+            }
+        }
+        static void GArray(ref int y)
+        {
+            Console.Write("Geben Sie an wie viele Module Sie haben: ");
+            y = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
